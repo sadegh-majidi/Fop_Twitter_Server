b078ea5258c3a6f164ee1174186cf64d2ad3415a
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-12-07 16:43:05 +0330

    request regex patterns

--- src/main/java/controller/RequestHandlingController.java
+++ src/main/java/controller/RequestHandlingController.java
@@ -7,7 +7,6 @@ public class RequestHandlingController {
     private TokenGenerator tokenGenerator;
 
     public RequestHandlingController() {
-        //hich
         tokenGenerator = new TokenGenerator();
     }
 
---
+++ src/main/java/server/RequestPatternType.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.util.regex.Pattern;
+
+public enum RequestPatternType {
+    SignUp(Pattern.compile("^signup\\s+(\\S+)\\s*,\\s*(\\S+)$")),
+    Login(Pattern.compile("^login\\s+(\\S+)\\s*,\\s*(\\S+)$")),
+    SendTweet(Pattern.compile("^send\\s+tweet\\s+(.+)\\s*,\\s*(.+)$")),
+    RefreshTimeLine(Pattern.compile("^refresh\\s+(.+)$")),
+    LikeTweet(Pattern.compile("^like\\s+(.+)\\s*,\\s*(\\d+)$")),
+    CommentTweet(Pattern.compile("^comment\\s+(.+)\\s*,\\s*(\\d+)\\s*,\\s*(.+)$")),
+    SearchUser(Pattern.compile("^search\\s+(.+)\\s*,\\s*(\\S+)$")),
+    Follow(Pattern.compile("^follow\\s+(.+)\\s*,\\s*(\\S+)$")),
+    UnFollow(Pattern.compile("^unfollow\\s+(.+)\\s*,\\s*(\\S+)$")),
+    Profile(Pattern.compile("^profile\\s+(.+)$")),
+    SetBio(Pattern.compile("^set\\s+bio\\s+(.+)\\s*,\\s*(.+)$")),
+    ChangePassword(Pattern.compile("^change\\s+password\\s+(.+)\\s*,\\s*(\\S+)\\s*,\\s*(\\S+)$"))
+    ;
+    private final Pattern pattern;
+
+    RequestPatternType(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+}
